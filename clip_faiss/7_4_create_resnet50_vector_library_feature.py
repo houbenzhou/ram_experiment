5b7443034e585a5808cfa632f79424b88968de8f
houbenzhou <houbenzhou@supermap.com>
2024-07-16 16:45:43 +0800

    创建基于resnet50向量库以及基于resnet50的查询日志

---
+++ clip_faiss/7_4_create_resnet50_vector_library_feature.py
@@ -0,0 +1,176 @@
+import os
+
+import cv2
+import faiss
+import numpy as np
+from PIL import Image
+import os
+
+import torch
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel, CLIPFeatureExtractor, AutoImageProcessor, AutoModel
+import json
+import faiss
+from tqdm import tqdm
+from pathlib import Path
+from torchvision.models import resnet50
+import torch
+from torchvision import transforms
+from torch import nn
+
+# 将图像缩放至同一尺寸
+# def resize(image_path, image_size=(256, 256)):
+#     img = Image.open(image_path)
+#     pass
+
+
+
+def resize(image_path, image_size=(256, 256)):
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, image_size)
+    # img = Image.fromarray(img).convert("RGB")
+    return img
+#创建颜色直方图
+def get_resnet50_features(image_path):
+    """
+
+    :return:
+    """
+    image = Image.open(image_path).convert('RGB')
+    image_features = resnet_model.embeding(image)
+    image_features = image_features.detach().numpy()
+    # image_features = outputs.last_hidden_state
+    # image_features = image_features.mean(dim=1)
+    # image_features = image_features.detach().numpy()
+    # image_features = outputs.last_hidden_state.detach().numpy()
+    return image_features
+
+def handle_img(img):
+    """
+    对HSV颜色空间中的V（亮度做一下均衡)
+    :param img:均衡后的图像，再转回BGR
+    :return:
+    """
+    # img = cv2.resize(img, (100, 100))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # 对HSV颜色空间中的V（亮度做一下均衡），再转会BGR
+    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+    return img
+
+
+def create_index(vector_dim, vectors):
+    # 创建一个索引
+    index = faiss.IndexFlatL2(vector_dim)  # 使用L2距离
+
+    # 添加向量到索引
+    if isinstance(vectors, np.ndarray):
+        index.add(vectors)
+    else:
+        index.add(np.array(vectors, dtype='float32'))
+
+    return index
+
+def extract_directory_name(path, level):
+    """
+    Extracts a directory name from a given path at the specified level.
+
+    :param path: The file path from which to extract the directory.
+    :param level: The level of the directory to extract, where 0 is the top-level.
+    :return: The name of the directory at the specified level or None if level is out of range.
+    """
+    try:
+        # 将字符串路径转换为Path对象
+        p = Path(path)
+        # 通过parts属性获取所有部分的元组，筛选出目录部分
+        # 忽略最后一部分如果它是文件名
+        directories=p.parts
+        # directories = [part for part in p.parts if Path(part).is_dir()]
+        # 返回指定层级的目录名
+        return directories[level]
+    except IndexError:
+        # 如果指定的层级不存在，返回None
+        return None
+class ResnetEmbeding:
+
+
+    def __init__(self,pretrained_model):
+        self.model = resnet50()
+        self.pretrained_model=pretrained_model
+        self.model.load_state_dict(torch.load(self.pretrained_model))
+
+        # delete fc layer
+        self.model.fc = nn.Sequential()
+        self.transform = transforms.Compose([transforms.Resize((224, 224)),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073],
+                                                                  std=[0.26862954, 0.26130258, 0.27577711])])
+
+    def embeding(self, image):
+        trans_image = self.transform(image)
+        trans_image = trans_image.unsqueeze_(0)
+        return self.model(trans_image)
+
+# 示例：使用颜色直方图创建FAISS索引
+if __name__ == "__main__":
+    base_path = os.getcwd()
+    # 图片路径
+    # image_path = os.path.join(base_path, 'data', 'clean_data_5037')
+    image_path = os.path.join(base_path, 'data', '新零售图片数据_Trax_部分')
+    # 向量长度
+    d = 2048
+    # 输出文件名
+    out_name = '35_category_05'
+
+    # resnet50_model的模型路径
+    resnet50_model_path = os.path.join(base_path, "model", "resnet_model","resnet50-0676ba61.pth")
+    id_type = 'image_path'
+
+    '''
+    生成图片faiss索引文件的路径
+        category_name:类别名称从文件夹路径中获取
+        image_path:类别名称是图片路径
+    '''
+
+    if id_type == 'category_name':
+        out_path = os.path.join(base_path, 'output', 'faiss_model', 'resnet50_feature',out_name)
+    elif id_type == 'image_path':
+        out_path = os.path.join(base_path, 'output', 'faiss_model', 'resnet50_feature',out_name)
+
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    out_category_name = os.path.join(out_path, 'category_name.json')
+    out_image_faiss=os.path.join(out_path, 'image_faiss.index')
+    # 加载resnet50模型
+
+    resnet_model = ResnetEmbeding(resnet50_model_path)
+
+    index = faiss.IndexFlatL2(d)  # 使用 L2 距离
+    # 遍历文件夹
+    file_paths = []
+    for root, dirs, files in os.walk(image_path):
+        for file in files:
+            # 检查文件是否为图片文件(这里简单地检查文件扩展名)
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                file_path = os.path.join(root, file)
+                file_paths.append(file_path)
+
+    if id_type == 'category_name':
+        id2filename = {idx: extract_directory_name(x,-2) for idx, x in enumerate(file_paths)}
+    elif id_type == 'image_path':
+        id2filename = {idx: x for idx, x in enumerate(file_paths)}
+
+    # 保存为 JSON 文件
+    with open(out_category_name, 'w') as json_file:
+        json.dump(id2filename, json_file)
+
+    for file_path in tqdm(file_paths, total=len(file_paths)):
+        # 使用PIL打开图片
+        image_features = get_resnet50_features(file_path)
+
+        index.add(image_features)
+
+
+    faiss.write_index(index, out_image_faiss)
+
+
